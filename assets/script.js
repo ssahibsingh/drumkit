4a1afe0f1c1296d1a8eaa288f82c07dbaa55e7d9
Sahib Singh <75523820+ssahibsingh@users.noreply.github.com>
2022-01-26 19:05:51 +0530

    Initial Commit

---
+++ assets/script.js
@@ -0,0 +1,41 @@
+var drumButton = document.querySelectorAll(".drum");
+var numberOfDrumButtons = drumButton.length;
+
+for (let i = 0; i < numberOfDrumButtons; i++) {
+    drumButton[i].addEventListener("click", function (){
+        var buttonInnerHTML = this.innerHTML
+        switch (buttonInnerHTML) {
+            case "w":
+                var tom1 = new Audio("assets/sounds/tom-1.mp3");
+                tom1.play();
+                break;
+            case "a":
+                var tom2 = new Audio("assets/sounds/tom-2.mp3");
+                tom2.play();
+                break;
+            case "s":
+                var tom3 = new Audio("assets/sounds/tom-3.mp3");
+                tom3.play();
+                break;
+            case "d":
+                var tom4 = new Audio("assets/sounds/tom-4.mp3");
+                tom4.play();
+                break;
+            case "j":
+                var snare = new Audio("assets/sounds/snare.mp3");
+                snare.play();
+                break;
+            case "k":
+                var kick = new Audio("assets/sounds/kick-bass.mp3");
+                kick.play();
+                break;
+            case "l":
+                var crash = new Audio("assets/sounds/crash.mp3");
+                crash.play();
+                break;
+        
+            default:
+                break;
+        }
+    })
+}
